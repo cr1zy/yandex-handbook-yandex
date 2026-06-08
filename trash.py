0210e5c7ebff66e3e5b87b89229a75b0515292f2
cr1zy <niki2005ta@gmail.com>
2026-06-08 20:21:15 +0300

    fuck the itertools a lot of time lost but codewars perfect

--- trash.py
+++ trash.py
@@ -1,7 +1,12 @@
-for index, value in enumerate("ABC", 2):
-    print(index, value)
+from itertools import product
 
-# Вывод:
-# 1 A  
-# 2 B  
-# 3 C
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'валет', 'дама', 'король', 'туз']
+suits = ['буби', 'пики', 'трефы', 'черви']
+suits.remove(input())
+cards.remove(input())
+suits_new = ['бубен', 'пик', 'треф', 'червей']
+
+for card, suit in product(cards, suits):
+    print(card, suit)
+
+# 10 mins
